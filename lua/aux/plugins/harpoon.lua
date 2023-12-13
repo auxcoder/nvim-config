5febec4e6164fc7d6854004db37c509e0fe7a021
Pavel Duran <auxcoder@gmail.com>
2023-12-13 18:35:03 -0500

    Upgrade harpoon plugin and config

--- lua/aux/plugins/harpoon.lua
+++ lua/aux/plugins/harpoon.lua
@@ -1,24 +1,29 @@
 return {
 	'ThePrimeagen/harpoon',
+	branch = 'harpoon2',
 	dependencies = {
 		'nvim-lua/plenary.nvim',
 	},
 	config = function()
-		-- set keymaps
-		local keymap = vim.keymap -- for conciseness
+		local k = vim.keymap
+		local h = require('harpoon')
 
-		keymap.set(
-			'n',
-			'<leader>hm',
-			"<cmd>lua require('harpoon.mark').add_file()<cr>",
-			{ desc = 'Mark file with harpoon' }
-		)
-		keymap.set('n', '<leader>hn', "<cmd>lua require('harpoon.ui').nav_next()<cr>", { desc = 'Go to next harpoon mark' })
-		keymap.set(
-			'n',
-			'<leader>hp',
-			"<cmd>lua require('harpoon.ui').nav_prev()<cr>",
-			{ desc = 'Go to previous harpoon mark' }
-		)
+		h:setup()
+
+		k.set('n', '<C-m>', function()
+			h:list():append()
+		end)
+
+		k.set('n', '<C-n>', function()
+			h.ui:nav_next()
+		end)
+
+		k.set('n', '<C-p>', function()
+			h.ui:nav_prev()
+		end)
+
+		k.set('n', '<C-t>', function()
+			h.ui:toggle_quick_menu(h:list())
+		end)
 	end,
 }
