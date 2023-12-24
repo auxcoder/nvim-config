eac70313e41fd97b23c28e8f67f6febea4bbfe28
Pavel Duran <auxcoder@gmail.com>
2023-12-24 16:02:57 -0500

    Fix config of better-scape.nvim

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -36,9 +36,16 @@ local plugins = {
 	{
 		'max397574/better-escape.nvim',
 		event = 'InsertEnter',
-		config = function()
-			require('better_escape').setup()
-		end,
+		opts = {
+			mapping = { 'jk', 'jj' }, -- a table with mappings to use
+			timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+			clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+			keys = '<Esc>', -- keys used for escaping, if it is a function will use the result everytime
+			-- example(recommended)
+			-- keys = function()
+			--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+			-- end,
+		},
 	},
 	{
 		'yamatsum/nvim-cursorline',
@@ -169,8 +176,7 @@ local plugins = {
 		'folke/trouble.nvim',
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		cmd = { 'Trouble', 'TroubleToggle', 'TodoTrouble' },
-		opts = {},
-		init = function()
+		opts = function()
 			require('custom.configs.trouble')
 		end,
 	},
@@ -192,7 +198,6 @@ local plugins = {
 		config = function()
 			require('custom.configs.symbols')
 		end,
-		-- config = true,
 	},
 	-- markdown preview
 	{
